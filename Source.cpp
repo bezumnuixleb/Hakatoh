5afa42cb64bfa4ca0b573e18b8546058e0faf597
Студент <@iipo.org>
2017-04-22 15:39:40 +0300

    kek9

--- Source.cpp
+++ Source.cpp
@@ -22,17 +22,26 @@ int main(int argc, char** argv)
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
 	SDL_RenderSetLogicalSize(renderer, WINDOW_SIZE_W, WINDOW_SIZE_H);
 	TTF_Font * font = TTF_OpenFont("eng.ttf", 25);//shrift
+	SDL_Surface* temp = IMG_Load("./wheels.png");
+	SDL_Texture* textwheel = addtext(renderer, temp);
+	temp = IMG_Load("./background.png");
+	SDL_Texture* backgroundwheel = addtext(renderer, temp);
+	temp = IMG_Load("./pointer.png");
+	SDL_Texture* pointer = addtext(renderer, temp);
+	SDL_Rect wheltextpos = { 0,0,900,900 };
+	SDL_Rect pointerPos = { 700,370,200,200 };
 	//text init
+	SDL_FreeSurface(temp);
 	bool isRunning = true;
-	int wind = 0,int vragnum;
+	int wind = 0, vragnum;
 	Player igrok;
-	SDL_Point mousepos;
+	SDL_Point mousepos = { 0,0 };
 	Vrag enemy;
 	int lastTicks = SDL_GetTicks();
 	SDL_SetRenderDrawColor(renderer, 255, 76, 0, 0);
-	int circlepos = 0,circletexturepos=0;
-	int tmpCub = 0, pressedbutonfight = -1;
-	float cubiklifetime = 0,rotatelifetime=0;
+	int circlepos = 0, circletexturepos = 0;
+	int tmpCub = 0, pressedbutonfight = -1, cubiknum = 0;
+	float cubiklifetime = 0, rotatelifetime = 0;
 	bool rotate = false;
 	initPlayer(&igrok);
 	while (isRunning)
@@ -45,109 +54,123 @@ int main(int argc, char** argv)
 				isRunning = false; return 0; break;
 			case SDL_KEYDOWN:switch (ev.key.keysym.sym)
 			{
-			
+
 			case SDLK_ESCAPE: {isRunning = false; return 0;} break;
 			}
-			
+
 			case SDL_MOUSEMOTION:
 			{
-				mousepos.x = ev.motion.x; mousepos.y = ev.motion.y; 
+				mousepos.x = ev.motion.x; mousepos.y = ev.motion.y;
 			}break;
-		}
-		if (wind==0)//вращение круга 
-		{
-			int currentTicks = SDL_GetTicks();
-			double dt = (currentTicks - lastTicks)*0.001;
-			lastTicks = currentTicks;
-
-			if (startispressed(mousepos)&&rotate==false)
-			{
-				tmpCub=getrand(1, 6);
-				circlepos += tmpCub;
-				//ogranichenie segmenta
-
-				cubiklifetime = 4000;
-				rotate == true;
 			}
-			if (rotate == true)
+			if (wind == 0)//вращение круга 
 			{
-				if (circletexturepos == circlepos)
+				int currentTicks = SDL_GetTicks();
+				double dt = (currentTicks - lastTicks)*0.001;
+				lastTicks = currentTicks;
+
+				if (startispressed(mousepos) && rotate == false)
 				{
-					rotate = false;
+					tmpCub = getrand(1, 6);
+					circlepos += tmpCub;
+					//ogranichenie segmenta
+
+					cubiklifetime = 4000;
+					rotate == true;
 				}
-				else
-				{	
-					if (rotatelifetime < 0)
+				if (rotate == true)
+				{
+					if (circletexturepos == circlepos)
 					{
-						circletexturepos++;
-						//povorot texturi kolesa na 1 segment
-						rotatelifetime = 2000;
+						rotate = false;
 					}
 					else
 					{
-						rotatelifetime -= dt;
+						if (rotatelifetime < 0)
+						{
+							circletexturepos++;
+							//povorot texturi kolesa na 1 segment
+							rotatelifetime = 2000;
+						}
+						else
+						{
+							rotatelifetime -= dt;
+						}
+					}
+					//ogranichenie segmenta
+
+					//
+				}
+				SDL_RenderCopy(renderer, backgroundwheel, NULL, NULL);//render zadnika
+				SDL_RenderCopy(renderer, textwheel, &wheltextpos,NULL );//render colesa
+				SDL_RenderCopy(renderer, pointer,NULL, &pointerPos);//render player
+				//render stats,money
+				if (cubiklifetime > 0)
+				{
+					cubiklifetime -= dt;
+					//SDL_RenderCopy(renderer, pointer, &pointerPos, NULL);
+				}//render cubika
+
+				SDL_RenderPresent(renderer);
+				SDL_RenderClear(renderer);
+			}
+			if (wind == 1)//окно боя
+			{
+				int currentTicks = SDL_GetTicks();
+				double dt = (currentTicks - lastTicks)*0.001;
+				lastTicks = currentTicks;
+				vragnum = getrand(0, 4);
+				initMob(&enemy, vragnum);
+				if (igrok.xod == true && enemy.xod == false)
+				{
+					draka_P(&igrok, &enemy, battleswitch(mousepos));
+				}
+				if (igrok.xod == false && enemy.xod == true)
+				{
+					SDL_Delay(200);
+					draka_V(&igrok, &enemy);
+				}
+				if (igrok.xod == false && enemy.xod == false)
+				{
+					if (igrok.hp > 0)
+					{
+						wind == 0;
 					}
 				}
-				//ogranichenie segmenta
-				
-			//
+				//render fon
+				//render player
+				//render mob (1-6)
+				//render stats
+				//
+				//
+
+				SDL_RenderPresent(renderer);
+				SDL_RenderClear(renderer);
 			}
-
-			//render zadnika
-			//render colesa
-			//render player
-			//render stats,money
-			//render cubika
-
-			SDL_RenderPresent(renderer);
-			SDL_RenderClear(renderer);
-		}
-		if (wind == 1)//окно боя
-		{
-			int currentTicks = SDL_GetTicks();
-			double dt = (currentTicks - lastTicks)*0.001;
-			lastTicks = currentTicks;
-			vragnum = getrand(0, 4);
-			initMob(&enemy, vragnum);
-			if (igrok.xod==true&&enemy.xod==false)
+			if (wind == 2)//главное меню
 			{
-				draka_P(&igrok, &enemy, battleswitch(mousepos));
+				int currentTicks = SDL_GetTicks();
+				double dt = (currentTicks - lastTicks)*0.001;
+				lastTicks = currentTicks;
+				glswitch(&wind, mousepos);
+				//render glmeny
+
+				SDL_RenderPresent(renderer);
+				SDL_RenderClear(renderer);
 			}
-			if (igrok.xod==false&& enemy.xod ==true)
+			if (wind == -1)
 			{
-				draka_V(&igrok, &enemy, 0);
+				isRunning = false; return 0; break;
 			}
-			
-			pressedbutonfight;
 
-
-			SDL_RenderPresent(renderer);
-			SDL_RenderClear(renderer);
 		}
-		if (wind == 2)//главное меню
-		{
-			int currentTicks = SDL_GetTicks();
-			double dt = (currentTicks - lastTicks)*0.001;
-			lastTicks = currentTicks;
-			glswitch(&wind,mousepos);
-			//render glmeny
-
-			SDL_RenderPresent(renderer);
-			SDL_RenderClear(renderer);
-		}
-		if (wind==-1)
-		{
-			isRunning = false; return 0; break;
-		}
-
 	}
 
+		SDL_DestroyRenderer(renderer);
+		SDL_DestroyWindow(window);
+		SDL_Quit();
 
-	SDL_DestroyRenderer(renderer);
-	SDL_DestroyWindow(window);
-	SDL_Quit();
-
-	return 0;
+		return 0;
 
 
 
